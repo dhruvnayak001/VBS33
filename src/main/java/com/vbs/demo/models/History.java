64537e67feacda486aa4df2a4a8f3664e23dabf7
dhruvnayak001 <it24.dhruv.nayak@kccemsr.edu.in>
2026-01-02 02:27:17 +0530

    Second Commit

--- src/main/java/com/vbs/demo/models/History.java
+++ src/main/java/com/vbs/demo/models/History.java
@@ -24,4 +24,10 @@ public class History {
     @CreationTimestamp
     @Column(nullable = false,updatable = false)
     LocalDateTime data;
+
+    @PrePersist                   //ye function call hona chaiye jab transaction succesfull ho jaye and transaction succesfull hone ke liye .. transaction class ka objkect bannana jaruri hai .... toh har object creation pe ye function trigger krneka kaam krta h prepersist annotatioon//
+    protected void onCreate(){
+        this.data = LocalDateTime.now();
+    }
+
 }
